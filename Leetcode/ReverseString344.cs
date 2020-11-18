03f02c97f11971376aefab789174f4cfefc03abe
Grigorii-Ilin <grigory1728@gmail.com>
2020-11-18 16:02:40 +0300

    Reverse String 344 solved

--- Leetcode/ReverseString344.cs
+++ Leetcode/ReverseString344.cs
@@ -3,9 +3,15 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Leetcode {
+    //https://leetcode.com/problems/reverse-string/
     class ReverseString344 {
         public void ReverseString(char[] s) {
-
+            int maxIndex = s.Length-1;
+            for (int i = 0; i < s.Length/2; i++) {
+                char tmp = s[i];
+                s[i] = s[maxIndex-i];
+                s[maxIndex - i] = tmp;
+            }
         }
     }
 }

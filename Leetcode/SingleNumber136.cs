317c0d049a53c1f8381aa6e7e8152d98c4d4cdd3
Grigorii-Ilin <grigory1728@gmail.com>
2020-11-18 18:54:46 +0300

    TopKFrequentElements347 done

--- Leetcode/SingleNumber136.cs
+++ Leetcode/SingleNumber136.cs
@@ -1,8 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Leetcode {
+    //https://leetcode.com/problems/single-number/
     class SingleNumber136 {
+        public int SingleNumber(int[] nums) {
+            return (from num in nums
+                    group num by num into g
+                    where g.Count() == 1
+                    select g.Key).First();
+        }              
     }
 }
---
+++ Leetcode/TopKFrequentElements347.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Leetcode {
+    //https://leetcode.com/problems/top-k-frequent-elements/
+    class TopKFrequentElements347 {
+        public int[] TopKFrequent(int[] nums, int k) {
+            return (from num in nums
+                    group num by num into g
+                    orderby g.Count() descending
+                    select g.Key).Take(k).ToArray();
+        }
+    }
+}
